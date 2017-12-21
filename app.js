8cd7df0353a1a1c64d4b34da53c5693fc5fadbe2
Sierra Moore <sierra.v.moore@gmail.com>
2017-12-21 11:06:23 -0600

    calculator shows from css and html

---
+++ app.js
@@ -0,0 +1,28 @@
+console.log("calculator!");
+
+let add = function(a,b){
+    return a + b;
+};
+let subtract = function(a,b){
+    return a - b;
+};
+let multiply = function(a,b){
+    return a * b;
+};
+let divide = function(a,b){
+    return a / b;
+};
+
+//array of operators
+let operators = ['+', '-', '/', '*'];
+
+
+
+
+
+
+
+// wrap all of the numbers in html in a class called numbers
+
+//then refer to that class and take two of those onclick events with class numbers
+// taking onlcick of numbers as a argument to math functions
